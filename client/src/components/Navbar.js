b126552b096e81524c314a0d10df8e76df2c09a0
MarcoDayz <diaz.marco8686@gmail.com>
2023-01-03 19:24:38 -0800

    inital commit

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const Navbar = () => {
+    const [input, setInput] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(input)
+        setInput('')
+    }
+
+    const handleChange = (e) => {
+        setInput(e.target.value)
+    }
+
+    return (
+        <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between'}}>
+            <h1>ShowFish</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    style={{width: '20rem'}}
+                    type={'text'}
+                    value={input}
+                    onChange={handleChange} />
+                <input type={'submit'} />
+            </form>
+            <div
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                gap: '1rem'}}>
+                <button>Join Now</button>
+                <button>Login</button>  
+            </div>
+        </div>
+    )
+};
+
+export default Navbar;
